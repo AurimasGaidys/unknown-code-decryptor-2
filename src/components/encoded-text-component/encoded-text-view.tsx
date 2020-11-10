86520ddc8a44c70711960833515f6c052556a94f
Aurimas Gaidys <aurimas.gaidys@gmail.com>
2020-11-10 21:17:03 +0200

    Notepad field added. Automatic suggestion VIP.

--- src/components/encoded-text-component/encoded-text-view.tsx
+++ src/components/encoded-text-component/encoded-text-view.tsx
@@ -1,5 +1,19 @@
 import React, { Component } from 'react';
+import { mostCommonWordsFilteredByLetters } from '../../data/most-comon-words';
 
 export const encodedDataView = (data: string, dataset: string[]) => {
-   return <div style={{display: "flex"}}>{data.split("-").map(x => <div style={{display: "flex", padding: 10}}>{ x.split(" ").map(y => <div style={{padding: 3}}>{dataset[parseInt(y)]}</div>)}</div>)}</div>
+   return <div style={{ display: "flex" }}>{data.split("-").map(x =>
+      <div style={{ display: "flex", padding: 10, flexDirection: "column" }}>
+         <select key={x} name="cars" id="cars" onChange={a => alert(a.target.value)}>
+            {mostCommonWordsFilteredByLetters(x.split(" ").length, x.split(" ").map(y => dataset[parseInt(y)])).map(x => <option value={x}>{x}</option>)}
+         </select>
+         <input />
+         <div style={{ display: "flex" }}>{x.split(" ").map(y =>
+            <div style={{ padding: 3 }}>{dataset[parseInt(y)] == "" ? y : dataset[parseInt(y)]}</div>
+         )}</div>
+      </div>)}</div>
+}
+
+export const overrideLetters = (newValue: string, data: string, dataset: string[]) => {
+
 }
--- src/components/main-container/main-container.tsx
+++ src/components/main-container/main-container.tsx
@@ -5,7 +5,7 @@ import { GetValueCounts } from '../../helpers/count-freqancy';
 
 export const MainContainer = () => {
 
-  const [dataset, setDataset] = useState(Array(27).fill(0).map((x, i) => "" + i));
+  const [dataset, setDataset] = useState(Array(27).fill(0).map((x, i) => ""));
   const data = "0-19 1 2 3 4 5 6-7 8 9-5 4 2 1-10 11 2 11 8-12 13 14 5 16 17 1 8 6-18 19 9 20 9-10 14 8 21 9 14 19 9 22";
   const data2 = "5 11 18 18 1-2 22 11 26-7 14 5 3 17";
   const data3 = "23 2 3 5 6-23 1 24 14 25 13 9 22 1 19 16-10 14 8 21 9 14 19 16-19 1-7 3 13 19 16 14-2 3 19 6-19 9 18 3 13";
@@ -24,7 +24,7 @@ export const MainContainer = () => {
         {encodedDataView(data2, dataset)}
         {encodedDataView(data3, dataset)}
       </div>
-      <div style={{ display: "flex", flexDirection: "column" }}>{dataset.map((x, i) => <div style={{fontSize: 16, padding: 3}}>{i}({result[i]}) <UseInput type="text" data={dataset} edit={setDataset} index={i} value={x} /></div>)}</div>
+      <div style={{ display: "flex", flexDirection: "column" }}>{dataset.map((x, i) => <div style={{fontSize: 16, padding: 3}}>{i}({result[i]}) <UseInput type="text" data={dataset} edit={setDataset} index={i} value={x} /><input></input></div>)}</div>
       {/* <p>{JSON.stringify(totalsArray)}</p> */}
       {/* <p style={{fontSize: 16}}>{JSON.stringify(result)}</p>  */}
     </div>)
